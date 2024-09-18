691758428b4a29bb51d9f6aeff2db0dbaf4a5194
sandroluisj <sandroluisjr06@gmail.com>
2024-09-18 16:59:52 -0300

    add

--- index.js
+++ index.js
@@ -68,6 +68,29 @@ if (TimeAtualizado) {
     res.status(500).json({ mensagem: "Erro na atualização do time", erro: erro.message });
 }
 });
+async function deletarTime(id) {
+    try {
+    const timeDeletado = await Time.findByIdAndDelete(id);
+    return timeDeletado;
+    } catch (erro) {
+    console.error("Erro ao deletar o time:", erro);
+    throw erro;
+    }
+    }
+    
+app.delete("/time/:id", async (req, res) => {
+    try {
+    const { id } = req.params;
+    const timeDeletado = await deletarTime(id);
+    if (timeDeletado) {
+    res.status(200).json({ mensagem: "Time deletado com sucesso", time: timeDeletado });
+    } else {
+    res.status(404).json({ mensagem: "O time não foi não encontrado..." });
+    }
+    } catch (erro) {
+    res.status(500).json({ mensagem: "Erro ao deletar o time", erro: erro.message });
+    }
+    });
 const port = 3000;
      app.listen(port, () => {
        console.log(`Servidor rodando na porta ${port}`);
