1d0421257f31a697deca7a816bce34c06f0486d1
antunessxx <kauanyantunes165@gmail.com>
2024-09-18 17:19:26 -0300

    add

--- registrarPartida.js
+++ registrarPartida.js
@@ -24,7 +24,32 @@ app.post('/times',async (req,res) => {
         const novoTime = new Time({nome,valor});
         await novoTime.save();
         res.status(201).json(novoTime);
-    } catch (error) {
-        res.status(400).send('Erro ao cadastrar time'+ error);
+        } catch (error) {
+        res.status(400).send('Erro ao cadastrar time'+ erro);
     }
 });
+
+app.post('/partida', async (req,res) => {
+        const {vencedorIndex, perdedorIndex} = req.body;
+        try{
+        const times = await time.find();
+        if(vencedorIndex < 0 || vencedorIndex >= times.length || perdedorIndex < 0 || perdedorIndex >= times.length){
+            return
+            res.status(400).send("Numero de time inválido");
+        }
+        let timeVencedor = times [vencedorIndex];
+        let timePerdedor = times [perdedorIndex];
+
+        if (timeVencedor.valor > timePerdedor.valor){
+            let temp = times [vencedorIndex];
+            times[vencedorIndex]= times [perdedorIndex];
+            times[perdedorIndex]= temp;
+
+            res.status(200).send(`partida registrada: ${timeVencedor.nome} venceu ${timePerdedor.nome}.`);
+        } else{
+            res.status(200).send(`partida registrada: ${timeVencedor.nome} venceu ${timePerdedor.nome}.`);
+        }
+    }catch (erro){
+        res.status(500).send('Erro ao registrar partida' + erro)
+    }
+})
